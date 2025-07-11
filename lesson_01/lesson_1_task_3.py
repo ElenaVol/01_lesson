d183e720936f00f2bacfb353964ee1c7ff6b2304
ElenaVol <alenushka_12-01@mail.ru>
2025-07-11 22:47:05 +0500

    выполнено ДЗ 2

--- lesson_01/lesson_1_task_3.py
+++ lesson_01/lesson_1_task_3.py
@@ -1,4 +1,5 @@
 first_name  = input("Как ваше имя? ")
-print(first_name)
 last_name = input("Как ваша фамиля?")
-print(first_name + " " +  last_name)
+print("Вас зовут: " + first_name + " " + last_name)
+
+ 
